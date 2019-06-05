e3be70b630eef782e515bbd34487c12f1385bf56
Ichinose Shogo <shogo82148@gmail.com>
2019-06-05 19:01:00 +0900

    add example project for adaptive icon

---
+++ example-adaptive/src/main/java/com/github/gfx/ribbonizer/example/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.github.gfx.ribbonizer.example;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)  {
+        new AlertDialog.Builder(this)
+                .setTitle("Hello, Android!")
+                .setMessage("This is an example app.")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+}
