3acd3bfb871ddefb7d3fb5bab92adf7b549c814e
Ichinose Shogo <shogo82148@gmail.com>
2021-05-17 21:37:06 +0900

    gradle init

--- ribbonizer-plugin/src/main/java/com/shogo82148/ribbonizer/resource/Filter.kt
+++
@@ -1,6 +0,0 @@
-package com.shogo82148.ribbonizer.resource
-
-interface Filter {
-    fun apply(icon: ImageAdaptiveIcon)
-    fun apply(icon: ImageIcon)
-}
--- ribbonizer-plugin/src/main/java/com/shogo82148/ribbonizer/resource/Resource.kt
+++
@@ -1,8 +0,0 @@
-package com.shogo82148.ribbonizer.resource
-
-import java.io.File
-
-abstract class Resource {
-    abstract fun apply(filter: Filter)
-    abstract fun save(dir: File)
-}
