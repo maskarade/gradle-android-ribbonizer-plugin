2ae0909ddc635e471f8e7d2a9e511892b96fedea
Ichinose Shogo <shogo82148@gmail.com>
2021-05-28 19:36:33 +0900

    add custom example

--- ribbonizer-plugin/src/main/kotlin/com/shogo82148/ribbonizer/plugin/RibbonizerExtension.kt
+++ ribbonizer-plugin/src/main/kotlin/com/shogo82148/ribbonizer/plugin/RibbonizerExtension.kt
@@ -3,7 +3,27 @@ package com.shogo82148.ribbonizer.plugin
 import com.shogo82148.ribbonizer.FilterBuilder
 
 open class RibbonizerExtension {
-    var iconNames: Set<String> = HashSet()
+    private var _iconNames: MutableSet<String> = HashSet()
+
+    val iconNames: Set<String>
+        get() = _iconNames
+
+    fun setIconNames(resNames: Collection<String>) {
+        _iconNames = HashSet(resNames)
+    }
+
+    fun iconNames(resNames: Collection<String>) {
+        _iconNames = HashSet(resNames)
+    }
+
+    fun iconNames(vararg resNames: String) {
+        _iconNames = HashSet(listOf(*resNames))
+    }
+
+    fun iconName(resName: String) {
+        _iconNames.add(resName)
+    }
+
     var forcedVariantsNames: Set<String> = HashSet()
     var filterBuilders: List<FilterBuilder> = ArrayList()
 
