118f616870d42a0ef25df28a2c264089abea2d88
Ichinose Shogo <shogo82148@gmail.com>
2021-12-12 16:25:14 +0900

    bump org.jetbrains.kotlin.jvm v1.6.0

--- ribbonizer-plugin/build.gradle.kts
+++ ribbonizer-plugin/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     `java-gradle-plugin`
 
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.5.31"
+    id("org.jetbrains.kotlin.jvm") version "1.6.0"
 
     // for publishing to Maven Central
     id("signing")
